0180044e7f16e819c393625d6dcb0036b11a5cf8
William Bellavance <flintman@flintmancomputers.com>
2018-01-23 12:39:08 -0500

    Revert "tenderloin-common: change TARGET_CPU_VARIANT to scorpion"
    This reverts commit 5fffb3c95795ba41e73eec19870ed3ccfa01c44e.
    
    Change-Id: I55f447bf16c919178cce6a8ae3ffbf29d86e84bd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := scorpion
+TARGET_CPU_VARIANT := cortex-a8
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8660
