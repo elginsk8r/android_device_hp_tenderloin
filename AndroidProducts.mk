c25f2450c5dfdc822d520058f2a1cf953f89fc46
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    tenderloin: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_tenderloin.mk \
     $(LOCAL_DIR)/ev_tenderloin.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_tenderloin-userdebug  \
+    ev_tenderloin-userdebug 
